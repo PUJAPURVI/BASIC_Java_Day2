0b4625f096a87d486fd52857da8c1d1e29fc32b2
PUJAPURVI <nerkarpuja@gmail.com>
2022-12-31 12:20:54 +0530

    Added: Sum Of Natural Number By Using For Loop

---
+++ ForLoopSumOfNaturalNumber.java
@@ -0,0 +1,21 @@
+package com.bridgelabzDay2;
+
+public class ForLoopSumOfNaturalNumber {
+    public static void main(String[] args)
+    {
+        int i, num = 20, sum = 0;
+//executes until the condition returns true
+        for(i = 1; i <= num; ++i)
+        {
+//adding the value of i into sum variable
+            sum = sum + i;
+        }
+//prints the sum
+
+        System.out.println("Sum of First 20 Natural Numbers is = " + sum);
+
+        System.out.println("sum of a+b");
+    }
+
+}
+
